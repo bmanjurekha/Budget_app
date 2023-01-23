23e0ccf26162f911bca31ddecaec73ea098ca250
Manjurekha <bmanjurekha@gmail.com>
2023-01-24 00:43:26 +0100

    SQL Script Changed

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,12 +1,11 @@
-CREATE DATABASE budget_app;
 USE budget_app;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE employee (
-                       id int AUTO_INCREMENT primary key,
-                       name varchar(255),
-                       created_at date,
-                       created_by varchar(32)
+                          id int AUTO_INCREMENT primary key,
+                          name varchar(255),
+                          created_at date,
+                          created_by varchar(32)
 );
 INSERT INTO employee (name, created_at, created_by) VALUES ("Emma", now(), "CONSOLE");
 INSERT INTO employee (name, created_at, created_by) VALUES ("Ulrika", now(), "CONSOLE");
